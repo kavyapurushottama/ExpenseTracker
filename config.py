018ac1fe11a57c83998f7a4d87721b99b907ed4f
Kavya <134777490+kavyapurushottama@users.noreply.github.com>
2025-04-01 12:02:31 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,3 @@
 SECRET_KEY = "your_secret_key_here"
-MONGO_URI = "mongodb+srv://kavya1112p:vAgMktVvHWTjVm5X@cluster0.3kxmq.mongodb.net/expense_tracker?retryWrites=true&w=majority"
-# MONGO_URI = "mongodb://localhost:27017"
-# import secrets
-# print(secrets.token_hex(32))
+MONGO_URI = "mongodb+srv://your_uri"
+
